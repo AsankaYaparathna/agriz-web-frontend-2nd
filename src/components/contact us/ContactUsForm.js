01a797c899658dd19b8b315d5e722691d575ba9e
Hirunasamadit <hirunasamardith@gmail.com>
2023-05-14 23:37:36 +0530

    contact us email js complete

--- src/components/contact us/ContactUsForm.js
+++ src/components/contact us/ContactUsForm.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { TextField, Button } from '@mui/material';
+import emailjs from 'emailjs-com';
 
 const ContactUsForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_mrw3164',
+        'template_yxg7ncp',
+        event.target,
+        'rBDyGuZfVuwyzuA1h'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    // Clear form fields after submission (optional)
+    event.target.reset();
+  };
+
   return (
-    <>
-      <TextField fullWidth label="Email" variant="outlined" margin="normal" />
+    <form onSubmit={handleSubmit}>
+      <TextField
+        fullWidth
+        label="Email"
+        variant="outlined"
+        margin="normal"
+        name="email"
+      />
       <TextField
         fullWidth
         label="Message"
@@ -12,15 +42,17 @@ const ContactUsForm = () => {
         margin="normal"
         multiline
         rows={4}
+        name="message"
       />
       <Button
+        type="submit"
         variant="contained"
         color="primary"
         sx={{ backgroundColor: '#064635', marginTop: 2 }}
       >
         Send
       </Button>
-    </>
+    </form>
   );
 };
 
